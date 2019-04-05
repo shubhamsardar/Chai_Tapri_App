b16f47d0939ac416d48a6ded52beb80aa65247ad
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-04-05 17:44:35 +0530

    Base URL changed

--- app/src/main/java/in/co/tripin/chai_tapri_app/Helper/Constants.java
+++ app/src/main/java/in/co/tripin/chai_tapri_app/Helper/Constants.java
@@ -2,7 +2,7 @@ package in.co.tripin.chai_tapri_app.Helper;
 
 public final class Constants {
 
-    public static final String BASE_URL = "http://206.189.135.19:3056/";
+    public static final String BASE_URL = "http://206.189.135.19:3057/";
    // public static final String BASE_URL = "http://d4c90d07.ngrok.io/";
     public static final String ADMIN_PHONE = "8394876737";
 
